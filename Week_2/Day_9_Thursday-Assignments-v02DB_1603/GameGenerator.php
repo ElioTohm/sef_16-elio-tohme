0cee937b06ad8252c3270420f377fc168e595249
Elio Tohme <eliotohme@gmail.com>
2016-07-30 10:31:34 +0300

    added GameGenerator PS: random target is still not ready

---
+++ Week_2/Day_9_Thursday-Assignments-v02DB_1603/GameGenerator.php
@@ -0,0 +1,33 @@
+<?php 
+// require_once
+class GameGenerator{
+	private	$BigNumberArr = array(25, 50, 75, 100);
+	private	$SmallNumberArr = array(1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10);
+	private	$TotalNumberInGame = 6;
+	private	$NumTakenFirstArray;
+	private	$NumTakenSecondArray;
+	private $GeneratedArray = array();
+	private $RandomTargetNumber;
+
+/*Generate random number taken from the first array, calculate the numbers to take fromt he second array and shuffle the 2 arrays for randomness*/
+	public function __construct(){
+		$this->NumTakenFirstArray = rand(1, 4);
+		$this->NumTakenSecondArray = $this->TotalNumberInGame - $this->NumTakenFirstArray;
+		shuffle($this->BigNumberArr);
+		shuffle($this->SmallNumberArr);
+	
+	}
+	
+/*push values from the shuffled arrays in a new array that will be returned*/
+ 	public function getGeneratedArray(){
+ 		for($i = 0; $i < $this->NumTakenFirstArray; $i++){			
+			array_push($this->GeneratedArray, $this->BigNumberArr[$i]);
+		}
+		for($i = 0; $i < $this->NumTakenSecondArray; $i++){
+			array_push($this->GeneratedArray, $this->SmallNumberArr[$i]);
+		}
+ 		return $this->GeneratedArray;
+ 	}
+ 
+}
+?>
