68bd84369f8358c3e43230d413b5c2ff42fda5f8
Elio Tohme <eliotohme@gmail.com>
2016-08-11 00:02:33 +0300

    Exercise 1 and 2 added ... they are simply the codes i used to build my database in 2 .sql files

---
+++ Week_4/Day_16_Wednesday-Assignments-v02BD_1603/Exercise_2.sql
@@ -0,0 +1,21 @@
+/*first create a view which will gather 
+ *and adds count while comparing with 
+ *the second procedure if that 1 has the same name*/
+
+CREATE VIEW procedures (procedure_id, compare_second_Procedure, total)
+AS SELECT P1.proc_id, P2.proc_id, COUNT(*)
+FROM AnestProcedures AS P1, AnestProcedures AS P2, AnestProcedures AS P3
+WHERE P2.anest_name = P1.anest_name
+AND P3.anest_name = P1.anest_name
+AND P1.start_time <= P2.start_time
+AND P2.start_time < P1.end_time
+AND P3.start_time <= P2.start_time
+AND P2.start_time < P3.end_time
+GROUP BY P1.proc_id, P2.proc_id;
+
+/*after creating the view i get the Max 
+ *of the column total and group tham by
+ *procedure_id*/
+SELECT procedure_id AS proc_id, MAX(total) AS max_inst_count
+FROM procedures
+GROUP BY procedure_id;
