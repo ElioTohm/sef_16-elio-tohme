cceff144bb00acadd255c20c6d93512398d806b5
Elio Tohme <eliotohme@gmail.com>
2016-08-12 17:24:18 +0300

    added using database

--- Week_4/Day_16_Wednesday-Assignments-v02BD_1603/Exercise_2.sql
+++ Week_4/Day_16_Wednesday-Assignments-v02BD_1603/Exercise_2.sql
@@ -1,7 +1,7 @@
 /*first create a view which will gather 
  *and adds count while comparing with 
  *the second procedure if that 1 has the same name*/
-
+use HospitalRecords;
 CREATE VIEW procedures (procedure_id, compare_second_Procedure, total)
 AS SELECT P1.proc_id, P2.proc_id, COUNT(*)
 FROM AnestProcedures AS P1, AnestProcedures AS P2, AnestProcedures AS P3
@@ -16,7 +16,7 @@ GROUP BY P1.proc_id, P2.proc_id;
 /*after creating the view i get the Max 
  *of the column total and group tham by
  *procedure_id*/
-use HospitalRecords;
+
 SELECT procedure_id AS proc_id, MAX(total) AS max_inst_count
 FROM procedures
 GROUP BY procedure_id;
