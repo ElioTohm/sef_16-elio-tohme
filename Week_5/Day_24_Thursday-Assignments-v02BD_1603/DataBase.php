7a0f204d4459e264d3aa27804850dfc9462e0a5c
Elio Tohme <eliotohme@gmail.com>
2016-08-19 15:20:42 +0300

    m so lost in the ignore file thing

--- Week_5/Day_24_Thursday-Assignments-v02BD_1603/DataBase.php
+++ Week_5/Day_24_Thursday-Assignments-v02BD_1603/DataBase.php
@@ -1,34 +1,25 @@
 <?php 
-require_once "Config.php";
-
 class DataBase
 {
 	private $CONN;
-	private $CONFIG;
 
-	function __construct() {
-		$this->CONFIG = new Config();
-   	}
-
-	function connect()
+	function __construct ($ServerName, $UserName, $Password, $DBName)
 	{
 		// Create connection
-		$this->CONN = new mysqli($servername, $username, $password, $dbname);
-		// Check connection
+		$this->CONN = mysqli_connect($ServerName, $UserName, $Password, $DBName);
 
-		if ($this->CONN->connect_error) {
-			die("Connection failed: " . $conn->connect_error);
-			} 
-		}
+		//check if connection 
+		if (mysqli_connect_errno()) {
+	        die( "Could not open connection to server" );
+	    }else{
+	    	echo "sdbfbdshf <br>";
+	    }
+	}
 	
-	function disconect()
+	function __destruct ()
 	{
-		$this->CONN->close();
+		// $this->CONN->close();
 	}
 
-	function executequery()
-	{
-		
-	}
 }
 ?>
--- Week_5/Day_24_Thursday-Assignments-v02BD_1603/Order.php
+++ Week_5/Day_24_Thursday-Assignments-v02BD_1603/Order.php
@@ -1,1 +1,17 @@
 <!DOCTYPE html>
+<meta charset="UTF-8">
+<?php 
+	require_once "DataBase.php"; 
+	require_once "Config.php"; 
+?>
+
+<html lang="en">
+<head>
+	<title>Exercise_1</title>
+	<body>
+		<h1>List of items</h1>
+		<?php 
+			$DB = new DataBase($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
+		?>
+	</body>
+</head>
