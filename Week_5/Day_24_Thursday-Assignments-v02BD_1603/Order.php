222799a6e0f65e5508433768b1e3ac11eaf099c1
Elio Tohme <eliotohme@gmail.com>
2016-08-20 12:11:29 +0300

    removed uneccessary classes added StyleSheet.css and file .gitignore to ignore the conf.php file

--- Week_5/Day_24_Thursday-Assignments-v02BD_1603/Order.php
+++ Week_5/Day_24_Thursday-Assignments-v02BD_1603/Order.php
@@ -1,17 +1,48 @@
 <!DOCTYPE html>
 <meta charset="UTF-8">
 <?php 
-	require_once "DataBase.php"; 
+	require_once "OrderProcess.php"; 
 	require_once "Config.php"; 
 ?>
 
 <html lang="en">
 <head>
+	<link rel="stylesheet" type="text/css" href="StyleSheet.css">
 	<title>Exercise_1</title>
 	<body>
 		<h1>List of items</h1>
-		<?php 
-			$DB = new DataBase($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
-		?>
+		<form action="action_page.php">
+			<fieldset>
+				<legend>Rent:</legend>
+				<label for="Name"> Name: </label>
+				<br>
+				<input id="Name" type="text" name="Name" placeholder="your first name and last name" required/>
+				<br>
+				<label for="title"> Title: </label>
+				<br>
+				<input id="title" type="text" name="Title" placeholder="title" required/>
+				<br>
+				<label for="Duration"> for: </label>
+				<br>
+				<input id="Duration"  name="Duration" type="date"  
+					data-date-inline-picker="true" placeholder="2016-11-23"
+					required/>
+				<br><br>
+				<input type="submit" value="Submit">
+			</fieldset>
+		</form>
+		<table>
+		<tr>
+			<th></th>
+			<th>Title</th>
+			<th>Year</th>
+			<th>Description</th>
+		</tr>
+			<?php 
+				$OrderProcess = new OrderProcess($SERVERNAME, $USERNAME, $PASSWORD, $DBNAME);
+				$OrderProcess->getAllMovies();
+			?>
+		</table>
+		
 	</body>
 </head>
--- Week_5/Day_24_Thursday-Assignments-v02BD_1603/OrderProcess.php
+++ Week_5/Day_24_Thursday-Assignments-v02BD_1603/OrderProcess.php
@@ -1,3 +1,50 @@
 <?php 
+class OrderProcess
+{
+	private $CONN;
 
+	function __construct ($ServerName, $UserName, $Password, $DBName)
+	{
+		// Create connection
+		$this->CONN = mysqli_connect($ServerName, $UserName, $Password, $DBName);
+
+		//check if connection 
+		if (mysqli_connect_errno()) {
+	        die( "Could not open connection to server" );
+	    }
+
+		if($_SERVER['REQUEST_METHOD'] == 'POST') {
+			echo "lol";
+		}
+	}
+	
+	function __destruct ()
+	{
+		if(is_resource($this->CONN))
+		{
+		    mysql_close($this->CONN);
+		}
+		
+	}
+
+	function getAllMovies()
+	{
+		// mysqli_query($this->CONN, "select * from film;");
+		$query = "SELECT title, release_year, description FROM film;";
+
+		if ($result = $this->CONN->query($query)) {
+
+			/* fetch object array */
+			while ($row = $result->fetch_row()) {
+				printf("<tr> <td><input type='checkbox' /></td> <td>%s</td> <td>%s</td> <td>%s</td> </tr>", $row[0], $row[1], $row[2]);
+			}
+		}
+	}
+
+	function rent()
+	{
+
+	}
+
+}
 ?>
