25baa6aa8ab8e5bc2cd88ea19347040768e2345b
Elio Tohme <eliotohme@gmail.com>
2016-08-30 14:58:48 +0300

    finished design started with functionality not quite done yet

--- Week_7/Day_29_Monday-Assignments-v02BD_1603/todo/js/main.js
+++ Week_7/Day_29_Monday-Assignments-v02BD_1603/todo/js/main.js
@@ -1,1 +1,19 @@
 
+var TodoItem = {
+	title : "",
+	timestamp: "",
+	detail : "",
+	addItem : function ()
+	{	
+		this.title = document.getElementById('title').value;
+		this.timestamp = Date.now();
+		this.detail = document.getElementById('details').value;
+		localStorage.setItem(this.title, JSON.stringify(this));
+	},
+	loadItems : function ()
+	{
+		for (var key in localStorage){
+   			console.log(key)
+		}
+	}
+}
