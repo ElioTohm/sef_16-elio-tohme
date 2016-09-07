98de30a9d69ca2406f0db0cd463024a188e41ef0
Elio Tohme <eliotohme@gmail.com>
2016-09-07 22:07:09 +0300

    first step showing posts

---
+++ Week_8/Day_36_Wednesday-Assignments-v02BD_1603/Blogster/app/Http/Controllers/PostController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+use App\Post;
+
+class PostController extends Controller
+{
+    public function get_allPosts ()
+    {
+		$post = Post::all();
+		return view('PostsView')->with('posts',$post);
+    }
+}
---
+++ Week_8/Day_36_Wednesday-Assignments-v02BD_1603/Blogster/resources/views/PostsView.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="panel panel-default">
+                <div class="panel-heading">Posts</div>
+                <div class="panel-body">
+				    <div class="row">
+				        <div class="col-md-8 col-md-offset-2">
+				            <div class="panel panel-default">
+								@foreach ($posts as $post)
+									<div class="panel-heading">{{ $post->title }}</div>
+						                <div class="panel-body">
+						                    {{ $post->body }}
+						                </div>
+									</div>
+								@endforeach
+				            </div>
+				        </div>
+				    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
